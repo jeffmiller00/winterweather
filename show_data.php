530e9ea78c0c6171e071f0b17c16194191e400e4
Jeff Miller <jeffmiller00@gmail.com>
2015-12-08 19:42:34 -0600

    Move data to external script to facilitate popup overs.

---
+++ show_data.php
@@ -0,0 +1,118 @@
+<?php
+$eventData = '[
+  {
+    "date":"2015-12-18",
+    "badge":false,
+    "title":"Winter Begins?",
+    "body":"Between December 18th and 20th is when the first winter weather of the season will come.",
+    "classname":"cold-1"},
+  {
+    "date":"2015-12-19",
+    "badge":false,
+    "title":"Winter Begins?",
+    "body":"Between December 18th and 20th is when the first winter weather of the season will come.",
+    "classname":"cold-1"},
+  {
+    "date":"2015-12-20",
+    "badge":false,
+    "title":"Winter Begins?",
+    "body":"Between December 18th and 20th is when the first winter weather of the season will come.",
+    "classname":"cold-1"},
+  {
+    "date":"2015-12-25",
+    "badge":false,
+    "title":"White Xmas?",
+    "body":"Kevin is leaning toward a cold rain w/ ice at the start, but you can bet he\'s hoping for a white Christmas.",
+    "classname":"cold-2"},
+  {
+    "date":"2015-12-31",
+    "badge":false,
+    "title":"First predicted snow on NYE",
+    "body":"Snow to ice to rain type event yielding 1\" - 3\" of snow",
+    "classname":"cold-3"},
+  {
+    "date":"2016-01-01",
+    "badge":false,
+    "title":"First predicted snow on NYE",
+    "body":"Snow to ice to rain type event yielding 1\" - 3\" of snow",
+    "classname":"cold-3"},
+  {
+    "date":"2016-01-15",
+    "badge":true,
+    "title":"Redskins 2nd Round Playoff Snow Storm",
+    "body":"The AO, NAO go negative, El nino creates an active southern jet stream, \'Skins at Carolina in the 2nd round of the playoffs that weekend and a 4\" - 8\" snow storm.",
+    "footer":"Empty the shelves of every supermarket in town -- this will absolutely impact travel.",
+    "classname":"cold-4"},
+  {
+    "date":"2016-01-16",
+    "badge":true,
+    "title":"Redskins 2nd Round Playoff Snow Storm",
+    "body":"The AO, NAO go negative, El nino creates an active southern jet stream, \'Skins at Carolina in the 2nd round of the playoffs that weekend and a 4\" - 8\" snow storm.",
+    "footer":"Empty the shelves of every supermarket in town -- this will absolutely impact travel.",
+    "classname":"cold-4"},
+  {
+    "date":"2016-01-17",
+    "badge":true,
+    "title":"Redskins 2nd Round Playoff Snow Storm",
+    "body":"The AO, NAO go negative, El nino creates an active southern jet stream, \'Skins at Carolina in the 2nd round of the playoffs that weekend and a 4\" - 8\" snow storm.",
+    "footer":"Empty the shelves of every supermarket in town -- this will absolutely impact travel.",
+    "classname":"cold-4"},
+  {
+    "date":"2016-01-27",
+    "badge":false,
+    "title":"A small, light freezing rain event.",
+    "classname":"cold-1"},
+  {
+    "date":"2016-01-28",
+    "badge":false,
+    "title":"A small, light freezing rain event.",
+    "classname":"cold-1"},
+  {
+    "date":"2016-02-06",
+    "badge":true,
+    "title":"The Dreaded Wraparound",
+    "body":"Marginal cold air in place when an Alberta Clipper shoots down from Canada, spawns a costal low off the Carolina, Virginia coastline. The storm starts as a mixture of rain and sleet, early in the weekend, then when the storm forms off the coast turns to HEAVY snow. It has potential to be a crippling storm to be measured in feet. We\'re looking at 12 to 18 inches, temps in teens, a wraparound with high winds, and blizzard conditions.",
+    "classname":"cold-4"},
+  {
+    "date":"2016-02-07",
+    "badge":true,
+    "title":"The Dreaded Wraparound",
+    "body":"Marginal cold air in place when an Alberta Clipper shoots down from Canada, spawns a costal low off the Carolina, Virginia coastline. The storm starts as a mixture of rain and sleet, early in the weekend, then when the storm forms off the coast turns to HEAVY snow. It has potential to be a crippling storm to be measured in feet. We\'re looking at 12 to 18 inches, temps in teens, a wraparound with high winds, and blizzard conditions.",
+    "classname":"cold-4"},
+  {
+    "date":"2016-02-12",
+    "badge":false,
+    "title":"A light ice event.",
+    "classname":"cold-1"},
+  {
+    "date":"2016-02-13",
+    "badge":false,
+    "title":"A light ice event.",
+    "classname":"cold-1"},
+  {
+    "date":"2016-02-14",
+    "badge":false,
+    "title":"A light ice event.",
+    "classname":"cold-1"},
+  {
+    "date":"2016-02-23",
+    "badge":false,
+    "title":"A light snow to ice event.",
+    "classname":"cold-1"},
+  {
+    "date":"2016-03-03",
+    "badge":false,
+    "title":"The Final Storm",
+    "body":"A cold air damming situation, as the storm develops in the gulf, 4-8\" of snow at the start, then it mixes and changes to freezing rain.",
+    "footer":"<div class=\'col-md-4\'><img width=\'100%\' src=\'https://www.att.com/olam/English/images/attLoader.gif?01AD=31PI4aGr-9VCjUJdTFuJGfq9TG0vtxmHmw_ROpAq8aOO7BFGtywezNg&amp;01RI=11A5C36296D582E&amp;01NA=\' /></div><div class=\'col-md-4\'><img width=\'100%\' src=\'http://www.jrbriggs.com/wp-content/uploads/2013/05/brand.gif\' /></div><div class=\'col-md-4\'><img width=\'100%\' src=\'http://www.bagelciti.com/wp-content/uploads/2013/09/bagel_city_logo-edit-small.png\' /><p class=\'tagline\'>Bagles are a lifestyle.</p></div>",
+    "classname":"cold-3"},
+  {
+    "date":"2016-12-31",
+    "badge":false,
+    "title":"Placeholder"
+  }
+]';
+
+
+echo $eventData;
+?>
